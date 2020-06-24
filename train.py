d9969ef25d3df876b22ae626a6a1faab7f0d8a6e
Harsh Desai <hurshd0@gmail.com>
2020-06-24 03:52:22 +0000

    chore: trying out github actions

---
+++ train.py
@@ -0,0 +1,21 @@
+# to handle datasets
+import pandas as pd
+import numpy as np
+
+# to divide train and test set
+from sklearn.model_selection import train_test_split
+
+# feature scaling
+from sklearn.preprocessing import StandardScaler
+
+# to build the models
+from sklearn.linear_model import LogisticRegression
+
+# to evaluate the models
+from sklearn.metrics import accuracy_score, roc_auc_score
+
+# to persist the model and the scaler
+import joblib
+
+
+data = pd.read_csv('titanic.csv')
